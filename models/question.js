8e138c39866996443972277adefb408ac0d41cc9
taranamol <tarskaur@gmail.com>
2015-09-01 09:03:03 -0700

    first commit

---
+++ models/question.js
@@ -0,0 +1,18 @@
+// models/question.js
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Answer = require('./answer');
+
+var QuestionSchema = new Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  answers: [Answer.schema]
+});
+
+var Question = mongoose.model('Question', QuestionSchema);
+
+module.exports = Question;
+
